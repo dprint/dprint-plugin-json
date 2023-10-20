86f50c4f49c85ddc4b66ca717f7eb5593298c74d
David Sherret <dsherret@gmail.com>
2023-10-20 16:10:30 +0900

    feat: trailingCommas: maintain

--- src/configuration/types.rs
+++ src/configuration/types.rs
@@ -5,17 +5,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, PartialEq, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub enum TrailingCommaKind {
-  /// Always format with trailing commas. (Beware: trailing commas can cause many JSON parsers to fail.)
+  /// Always format with trailing commas. Beware: trailing commas can cause many JSON parsers to fail.
   Always,
-  /// Never format with trailing commas.
-  Never,
   /// Use trailing commas in JSONC files and do not use trailing commas in JSON files. (Default)
   Jsonc,
+  /// Keep the trailing comma if it exists.
+  Maintain,
+  /// Never format with trailing commas.
+  Never,
 }
 
 generate_str_to_from![
   TrailingCommaKind,
   [Always, "always"],
+  [Maintain, "maintain"],
   [Never, "never"],
   [Jsonc, "jsonc"]
 ];
